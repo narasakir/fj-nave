2686567ff1362a991c6ff4cc1fd2c569338bfd11
Raphael Narasaki <narasakir@gmail.com>
2018-01-17 14:27:52 -0200

    Criando layout

--- js/opcoesDaPagina/btnMudaLayout.js
+++ js/opcoesDaPagina/btnMudaLayout.js
@@ -8,5 +8,24 @@ function mudaTexto(){
         this.textContent = 'Blocos'
     }
 }
-btn.onclick = mudaTexto //Função de callback , chama a função sem executar
+btn.addEventListener('click', mudaTexto) 
+//   btn.onclick = mudaTexto    Função de callback , chama a função sem executar
+
+//Adicionar a classe
+const mural = document.querySelector('.mural')
+
+function mudaLayout(){
+    mural.classList.toggle('mural--linha')
+
+    // o método .toggle() executa a estrutura de condição 
+    /*
+    if(mural.classList.contains('mural--linha)){
+        mural.classList.remove('mural--linha')
+    }else{
+        mural.classList.add('mural-linha)
+    }
+    */
+}
+
+btn.addEventListener('click', mudaLayout)
 
