296816ec5a0f831aa71439d498935e4feca7fe7b
Raphael Narasaki <narasakir@gmail.com>
2018-01-19 15:19:14 -0200

    Refatorando: jQuery para criar o cartao e eventos

--- js/formNovoCartao.js
+++ js/formNovoCartao.js
@@ -24,9 +24,8 @@
             })
         }else{
             contador++
-            const $tpl = document.createElement('tpl') // Criando o elemento como uma template string
-            //Adicionando o conteudo e deixando dinamico
-            $tpl.innerHTML = ` 
+            //Adicionando o conteudo e deixando dinamico com jQuery
+            const $cartao = $(` 
                     <article id="cartao_${contador}" class="cartao" tabindex="0">
                     <div class="opcoesDoCartao">
                     <button class="opcoesDoCartao-remove opcoesDoCartao-opcao" tabindex="0">
@@ -56,9 +55,41 @@
                     <p class="cartao-conteudo" contenteditable tabindex="0">${$conteudo}</p>
                 </article>
             
-            `
-            const $cartao = $tpl.querySelector('.cartao') // Retirei o article dentro da tag tpl
-            document.querySelector('.mural').insertAdjacentElement('afterbegin', $cartao) // Adicionando dentro do mural
+            `)
+
+
+
+            $cartao.on('focusin', function(){
+                $cartao.addClass('cartao--focado')
+            })
+            
+            $cartao.on('focusout', function(){
+                $cartao.removeClass('cartao--focado')
+            })
+    
+            // Delegate para trocar a cor
+            $cartao.on('change', function(event){ // Verifica quando o evento for disparada, neste caso, a mudança do estado. 
+                const $elementoAtual = $(event.target)  // Pega os eventos do elemento atual        
+                    const novaCor = $elementoAtual.val() // Pega os eventos do elemento atual || Quando pegar um elemento da página sempre adicionar $ na variável
+                    $cartao.css('background', novaCor)
+            })
+            // Delegate para trocar a cor ao teclar
+            $cartao.on('keyup', '.opcoesDoCartao-opcao',  function(event){
+                const  $elementoAtual = $(event.target) // Pega os eventos do elemento atual 
+                if(event.key === 'Enter' || event.key === ' '){ // verifica se é o elemento e o evento desejado.
+                    $elementoAtual.click()
+                }
+            })
+            // Delegate para remover o cartão
+            $cartao.on('click', '.opcoesDoCartao-remove', function(event){ // No jQuery faz delegate acoplado
+                const $elementoAtual = $(event.target)
+                $cartao.addClass('cartao--somePeixinho')
+                $cartao.on('transitionend', function(){
+                    $cartao.remove();
+                })   
+                
+            })
+            $('.mural').prepend($cartao) // Adicionando dentro do mural
         }
 
     })
